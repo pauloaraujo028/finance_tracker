2dabfce3ad06553b1daafe53b1ebb200fa20e132
Paulo Araújo <pauloaraujo.phz@gmail.com>
2025-02-27 16:31:07 -0300

    feat: add componente logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,10 @@
+import Logo from "@/components/logo";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative flex w-full h-screen flex-col items-center justify-center">
+      <Logo />
       <div className="mt-12">{children}</div>
     </div>
   );
