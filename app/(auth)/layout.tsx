1f1ab3e239dcd9103128455a9b2f999f6fa450e9
Paulo Araújo <pauloaraujo.phz@gmail.com>
2025-02-28 09:47:40 -0300

    chore: ajuste na exportação do componente logo

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import Logo from "@/components/logo";
+import { Logo } from "@/components/logo";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 import { PiggyBank } from "lucide-react";
 import Link from "next/link";
 
-const Logo = () => {
+export const Logo = () => {
   return (
     <Link href="/" className="flex items-center gap-2">
       <PiggyBank className="stroke size-11 stroke-amber-500 stroke-[1.5]" />
@@ -12,4 +12,12 @@ const Logo = () => {
   );
 };
 
-export default Logo;
+export const LogoMobile = () => {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Finance Tracker
+      </p>
+    </Link>
+  );
+};
